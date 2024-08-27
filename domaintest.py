87eee412941768757841999059ef0bd0391e3af2
code4reis <gabriel_qrv@hotmail.com>
2024-08-27 17:21:13 -0300

    v1.0.2

--- domaintest.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/domaintest')
-def domaintest():
-    data = request.json
--- main.py
+++ main.py
@@ -6,5 +6,6 @@ CORS(app)
 
 if __name__ == '__main__':
     from speed import*
+    from domaintest import *
     # init_db()
     app.run(host='192.168.128.57', port=5000, debug=True)

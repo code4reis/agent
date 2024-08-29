8dcf087d6a19d6e9437da2f9c7bd11018715c9c9
code4reis <gabriel_qrv@hotmail.com>
2024-08-29 11:46:49 -0300

    v1.0.5

---
+++ config.py
@@ -0,0 +1,9 @@
+CLIENT_IP="192.168.128.57" # Ip do agent do cliente
+CLIENT_PORT="5000" # Porta utilizada pela aplicação
+CLIENT_PROTOCOL="http" # Protocolo de comunicação, http se  SSL_CERTIFICATE = False, se não https
+DEBUG_MODE=True # Ativado = True, Desativado = False
+SSL_CERTIFICATE=False #Ativado = True, Desativado = False
+
+# Se SSL_CERTIFICATE = True:
+CRT_PATH="" # Caminho do arquivo .crt
+KEY_PATH="" # Caminho do arquivo .key
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from config import *
 from flask import Flask
 from flask_cors import CORS
 
-
+# Instancia Flask
 app = Flask(__name__)
+
+# CORS para permitir requisições de mesma origem http
 CORS(app)
 
+# Páginas
 from speed import *
 from bandmonitor import *
 
 if __name__ == '__main__':
     iniciar_monitoramento()
-    app.run(host='192.168.128.57', port=5000, debug=True)
+
+    ssl_context = None
+    if SSL_CERTIFICATE:
+        ssl_context = (CRT_PATH, KEY_PATH)
+
+    app.run(
+        host=CLIENT_IP,
+        port=int(CLIENT_PORT),
+        ssl_context=ssl_context,
+        debug=True
+    )
